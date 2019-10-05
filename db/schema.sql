3e172a06258c240426b071c90d5903637bff30a3
mhayes428 <fikes428@gmail.com>
2019-10-05 15:52:54 -0400

    create table

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    deleted BOOLEAN DEFAULT false,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
