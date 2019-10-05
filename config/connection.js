805f521a34cae48dd50f42a5bf6c2b080acfea89
mhayes428 <fikes428@gmail.com>
2019-10-05 19:50:27 -0400

    password

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "2541",
+        password: process.env.MYSQL_PASSWORD,
         database: "burgers_db"
 
     });
